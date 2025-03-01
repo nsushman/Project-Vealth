1e9c0137eb575454ebf0547a34591f1b6f608ba4
Sush <nsushman@gmail.com>
2025-03-01 07:58:26 +0000

    Initial commit - Upload Project-Vealth

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import './App.css'
+import './App.css';
 
 export default function App() {
   return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit
+    <main className="container mt-5">
+      <h1 className="text-center text-primary">Hello, World!</h1>
     </main>
-  )
+  );
 }
